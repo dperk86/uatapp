be5b8a645c3632c540dfce099c79098cc3a312ca
Demetrick Perkins <demetrick.perkins@mediahive.com>
2017-06-16 13:31:20 -0400

    adds page 3 to navigation

--- src/navigator.js
+++ src/navigator.js
@@ -2,11 +2,13 @@ import { StackNavigator } from 'react-navigation';
 
 import Home from './containers/home';
 import Page2 from './containers/page2';
+import Page3 from './containers/page2s';
 
 const Navigator = StackNavigator(
   {
     Home: { screen: Home },
     Page2: { screen: Page2 },
+		Page3: { screen: Page3 },
   },
   {
     navigationOptions: {
